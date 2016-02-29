be95ebb4fb40288350754b6b0dc7690f2a42cb53
Matic Jurglic <matic@jurglic.si>
2016-02-29 22:08:12 +0100

    Fetch user after login, save it into store and set it to session

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,25 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import ENV from "../config/environment";
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  session: Ember.inject.service(),
+  authenticatedAjax: Ember.inject.service(),
+  sessionAuthenticated() {
+    this._super(...arguments); // for keeping the defaults (attempted transition, routeAfterAuth config etc..)
+    this.loadUser();
+  },
+  loadUser() {
+    if (this.get('session.isAuthenticated')) {
+      const request = this.get('authenticatedAjax').request(`${ENV.apiBaseURL}/users/me`);
+      request.then((userData) => {
+        this.store.pushPayload(userData);
+        const user = this.store.peekRecord('user', userData.data.id);
+        this.set('session.currentUser', user);
+      });
+    }
+  },
+  beforeModel() {
+    this.loadUser();
+  }
 });
