ecce7dd3eb78b4eef59914eaeb60fe1e754170c6
Matic Jurglic <matic@jurglic.si>
2016-03-06 11:17:38 +0100

    Display some sample data (balance changes) in overview

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,8 +3,34 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service(),
+  beforeModel(transition) {
+    this._super(...arguments);
+    if (transition.targetName === "dashboard.index"){
+      transition.abort();
+      this.transitionTo('dashboard.overview');
+    }
+  },
+  model() {
+    return [
+      {
+        value: 310,
+        changeType: "income",
+        entryDate: "12-12-2015"
+      },
+      {
+        value: 400,
+        changeType: "expense",
+        entryDate: "12-12-2015"
+      },
+      {
+        value: 245,
+        changeType: "expense",
+        entryDate: "12-12-2015"
+      }
+    ];
+  },
   actions: {
-    logout () {
+    logout() {
       this.get('session').invalidate();
     }
   }
