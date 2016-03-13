32fb38156a062b20dffc08b9d63b5db08f355f04
Matic Jurglic <matic@jurglic.si>
2016-03-13 20:52:21 +0100

    Computed properties, overview, expenses, incomes

--- app/controllers/dashboard/expenses/index.js
+++ app/controllers/dashboard/expenses/index.js
@@ -2,5 +2,4 @@ import Ember from 'ember';
 import BalanceChangePropertiesMixin from 'red-green-tutorial/mixins/balance-change-properties';
 
 export default Ember.Controller.extend(BalanceChangePropertiesMixin, {
-  session: Ember.inject.service()
 });
--- app/controllers/dashboard/overview.js
+++ app/controllers/dashboard/overview.js
@@ -1,6 +1,4 @@
 import Ember from 'ember';
 import BalanceChangePropertiesMixin from 'red-green-tutorial/mixins/balance-change-properties';
 
-export default Ember.Controller.extend(BalanceChangePropertiesMixin, {
-  session: Ember.inject.service()
-});
+export default Ember.Controller.extend(BalanceChangePropertiesMixin);
