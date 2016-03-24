1aea6bb99aaefd499e6ce0fb9113b456933f16c2
Matic Jurglic <matic@jurglic.si>
2016-03-24 20:17:54 +0100

    Add deleting of balance changes

--- app/controllers/dashboard/expenses/index.js
+++ app/controllers/dashboard/expenses/index.js
@@ -2,4 +2,11 @@ import Ember from 'ember';
 import BalanceChangePropertiesMixin from 'red-green-tutorial/mixins/balance-change-properties';
 
 export default Ember.Controller.extend(BalanceChangePropertiesMixin, {
+  actions: {
+    delete(balanceChange) {
+      if (confirm("Are you sure?")) {
+        balanceChange.destroyRecord().then(() => this.send('refreshRoute'));
+      }
+    }
+  }
 });
