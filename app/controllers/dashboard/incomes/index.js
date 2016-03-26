365da9c2887182d9b5d61b42f888216d26810754
Matic Jurglic <matic@jurglic.si>
2016-03-26 12:55:28 +0100

    Add currencyData service and use it to display user's currency symbol

--- app/controllers/dashboard/incomes/index.js
+++ app/controllers/dashboard/incomes/index.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import BalanceChangePropertiesMixin from 'red-green-tutorial/mixins/balance-change-properties';
 
 export default Ember.Controller.extend(BalanceChangePropertiesMixin, {
+  session: Ember.inject.service(),
   actions: {
     delete(balanceChange) {
       if (confirm("Are you sure?")) {
--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,10 @@ import DS from 'ember-data';
 import Ember from 'ember';
 
 export default DS.Model.extend({
+  currencyData: Ember.inject.service(),
   email: DS.attr('string'),
-  currency: DS.attr('string')
+  currency: DS.attr('string'),
+  currencySymbol: Ember.computed('currency', function() {
+    return this.get('currencyData.currencies')[this.get('currency')].symbol;
+  })
 });
