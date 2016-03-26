fc49b52f23213ec37d31e817d147556fc65a1df8
Matic Jurglic <matic@jurglic.si>
2016-03-26 12:52:45 +0100

    Add monthly overview chart

--- app/controllers/dashboard/overview.js
+++ app/controllers/dashboard/overview.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 import BalanceChangePropertiesMixin from 'red-green-tutorial/mixins/balance-change-properties';
 
-export default Ember.Controller.extend(BalanceChangePropertiesMixin);
+export default Ember.Controller.extend(BalanceChangePropertiesMixin, {
+  dashboard: Ember.inject.controller(),
+  period: Ember.computed.alias('dashboard.period'),
+  avgExpensePerDay: Ember.computed('expenseSum', function() {
+    const dateParts = this.get('period').split("-");
+    const year = dateParts[0];
+    const month = dateParts[1];
+    const daysInMonth = new Date(year, month, 0).getDate();
+    return this.get('expenseSum')/daysInMonth;
+  })
+});
